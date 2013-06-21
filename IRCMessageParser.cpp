699207ea514922bfb02a0a7203e2703c058842a0
Buenyamin Eskiocak <buenyamin.eskiocak@r4apps.com>
2013-06-22 00:31:25 +0200

    IRC Message Parser trennt nun auch bei Doppelpunkten

--- IRCMessageParser.cpp
+++ IRCMessageParser.cpp
@@ -11,12 +11,13 @@ IRCMessageObject IRCMessageParser::ParseMessage(string message, bool &error) {
         return msgObj;
     }
 
+    // parse sender of message
     msgObj.sender = message.substr(++pos1, pos2-pos1-1);
-    cout << "Sender: " << msgObj.sender << endl;
 
     pos1 = message.find(" ");
     pos2 = message.find(" ", pos1+1);
 
+    // parse type of message
     string msgType = message.substr(++pos1, pos2-pos1-1);
     if (msgType == "PRIVMSG")       msgObj.type = IRC_PRIVMSG;
     else if (msgType == "JOIN")     msgObj.type = IRC_JOIN;
@@ -28,6 +29,8 @@ IRCMessageObject IRCMessageParser::ParseMessage(string message, bool &error) {
         error = true;
         return msgObj;
     }
+
+    // parse all message parts
     string msgPart;
     int partNo = 0;
     pos1 = pos2;
@@ -37,15 +40,16 @@ IRCMessageObject IRCMessageParser::ParseMessage(string message, bool &error) {
             IRCMessageParser::CheckAndAddPart(partNo++, msgPart, msgObj);
         } pos1 = pos2;
     }
+    // parse last message part
     msgPart = message.substr(++pos1, pos2-pos1-1);
     if (!msgPart.empty())
         IRCMessageParser::CheckAndAddPart(partNo, msgPart, msgObj);
 
-    cout << endl << "CHANNEL: " << msgObj.channel << endl << "RECEIVER: " << msgObj.receiver << endl << endl;
-    for (vector<string>::iterator i=msgObj.message.begin(); i!=msgObj.message.end(); i++) {
-        cout << "MSG-PART " << ": " << *i << "|||" << endl;
-    }
     cout << endl << endl;
+    for (vector<string>::iterator i=msgObj.message.begin(); i!=msgObj.message.end(); i++)
+        cout << "PART: " << *i << "|||" << endl;
+    cout << endl << endl;
+
     error = false;
     return msgObj;
 }
@@ -53,10 +57,27 @@ void IRCMessageParser::CheckAndAddPart(int partNo, string msgPart, IRCMessageObj
     if (partNo == 0) {
         if (msgPart.at(0) == '#')   msgObj.channel = msgPart.substr(1);
         else                        msgObj.receiver = msgPart;
-    } else if (partNo == 1 && msgPart.at(0) == ':') {
-        msgObj.message.push_back(msgPart.substr(1));
+        return;
+    }
+    if (partNo == 1 && msgPart.at(0) == ':')
+        msgPart = msgPart.substr(1);
+
+    if (msgPart.find(":") != string::npos) {
+        size_t pos1, pos2;
+        string tmpPart;
+        pos1 = 0;
+        while ((pos2 = msgPart.find(":", pos1+1)) != string::npos) {
+            if (pos2 != 0) {
+                tmpPart = msgPart.substr(pos1, pos2-pos1+1);
+                msgObj.message.push_back(tmpPart);
+                if (pos2 == msgPart.length()-1)
+                    break;
+            } pos1 = pos2 + 1;
+        } if (pos2 == string::npos) {
+            tmpPart = msgPart.substr(pos1, msgPart.length()-pos1);
+            msgObj.message.push_back(tmpPart);
+        }
     } else {
         msgObj.message.push_back(msgPart);
     }
 }
-
